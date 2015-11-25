741c7f4926bd3746be776f64b77e361e31ecb2e8
artven <rafal.prusak@gmail.com>
2015-11-25 19:04:35 +0100

    Drobna zmiana na stronie głównej gui

--- gui/server.R
+++ gui/server.R
@@ -1,6 +1,20 @@
 library(shiny)
 
 shinyServer(function(input, output) {
+    
+    # wypisanie próbnego tekstu 
+    output$gender_output <- renderText({ 
+        "Pleć osoby"
+    })
+    output$gender_probability_output <- renderText({ 
+        "Prawdopodobieństwo wyznaczenia płci"
+    })
+    output$age_output <- renderText({ 
+        "Wiek osoby"
+    })
+    output$age_probability_output <- renderText({ 
+        "Prawdobodobieństwo wyznaczenia płci"
+    })
 })
 
         
--- gui/ui.R
+++ gui/ui.R
@@ -15,8 +15,8 @@ shinyUI(
                         style = "text-align: center;",
                         actionButton("action", label = "Katarzyna Hejke-Gójka", width='20%'),    
                         actionButton("action", label = "Cezary Gmyz", width='20%'),
-                        actionButton("action", label = "Renata Gargas", width='20%'),
-                        actionButton("action", label = "Piotr Szumlewicz", width='20%')
+                        actionButton("action", label = "Piotr Szumlewicz", width='20%'),
+                        actionButton("action", label = "Uruchom testy", width='20%')
                     )
                 )
             ),
@@ -25,8 +25,7 @@ shinyUI(
                 style = "text-align: center; font-family: monospace; color: #888;",
                 div(
                     style = "text-align: justify; background-color: #f5f5f5; width: 75%; border: 1px solid #e3e3e3; border-radius: 10px;  display: inline-block; padding: 10px;",
-                    h4(strong("Ma Pan dowód?")),
-                    p("Jeżeli nie, to wczytaj sobie własny plik."),
+                    h4(strong("Wczytaj własny plik")),
                     div(
                         style = "text-align: center;",
                         fileInput("file", label = "")
@@ -39,17 +38,21 @@ shinyUI(
                 div(
                     style = "text-align: justify; background-color: #f5f5f5; width: 75%; border: 1px solid #e3e3e3; border-radius: 10px;  display: inline-block; padding: 10px;",
                     h4(strong("Rezultat:")),
-                    p("Tutaj będzie się pojawiał wykres i może jakieś pola tekstowe?"),
                     div(
                         style = "text-align: center;",
-                        p("wykres x od y"),
-                        img(src = "testImg.jpg", width="50%", height="50%"),
                         fluidRow(
                             column(3, 
-                                textInput("text", label = "Płeć osoby", value = "Tajlor Swift")
+                                "",
+                                textOutput("gender_output")
+                            ),
+                            column(3, 
+                                textOutput("gender_probability_output")
                             ),
                             column(3,
-                                textInput("text", label = "Wiek osoby", value = "25(?)")    
+                                textOutput("age_output")    
+                            ),
+                            column(3,
+                                textOutput("age_probability_output")    
                             )
                         )
                     )
@@ -140,52 +143,5 @@ shinyUI(
     )
 )
 
-
-
-
-# shinyUI(
-#   fluidPage(
-#     titlePanel("Program SG")
-#   )
-# )
-
-# shinyUI(fluidPage(
-#   titlePanel("Program SG"),
-#   helpText("Przedmiot: systemy głosowe, temat projektu: analiza czegoś tam(uzupełnić), grupa: 3, kierunek: Automatyka i Robotyka", 
-#            "szef grupy: Adam Giża, plebs: reszta członków."),
-# 
-#   fluidRow(
-#     
-#     column(4,
-#       h3("Wczytaj plik"),
-#       p("Wybierz własny plik do analizy."),
-#       fileInput("file", label = ""),
-#       h3("Przykładowe próbki głosu"),
-#       p("Uruchom analizę głosu znanych osób."), br(),
-#       actionButton("action", label = "Katarzyba Hejke-Gójka", width='200px'), br(),
-#       actionButton("action", label = "Cezary Gmyz", width='200px'), br(),
-#       actionButton("action", label = "Adam Małysz", width='200px'), br(),
-#       actionButton("action", label = "Renata Gargas", width='200px'), br(),
-#       actionButton("action", label = "Lech Wałęsa", width='200px')
-#     ),
-#     
-#     column(3,
-#       h3("Opcje"),
-#       checkboxInput("checkbox", label = "Opcja 1", value = TRUE),
-#       checkboxInput("checkbox", label = "Opcja 2", value = TRUE),
-#       checkboxInput("checkbox", label = "Opcja 3", value = TRUE),
-#       radioButtons("radio",  label = "", choices = list("Opcja 4" = 1, "Opcja 5" = 2, "Opcja 6" = 3), selected = 1),
-#       sliderInput("Opcja 7", label = "Opcja 7", min = 20, max = 100, value = 50),
-#       sliderInput("Opcja 8", label = "Opcja 8", min = 0, max = 1, value = 0.5),
-#       numericInput("num", label = "Opcja 9", value = 1),
-#       selectInput("select", label = "Opcja 10", choices = list("Choice 1" = 1, "Choice 2" = 2, "Choice 3" = 3), selected = 1)
-#     ),
-#     
-#     column(3,
-#       h3("Wykres"),
-#       plotOutput("wykres tu będzie")
-#     )
-#   )
-# ))
 
  
