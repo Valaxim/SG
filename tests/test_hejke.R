69d08726eaf4ceca8e296fb54b0f4edf7bcfb980
artven <rafal.prusak@gmail.com>
2015-11-25 22:19:06 +0100

    Testowe skrypty dla buttonów.

---
+++ tests/test_hejke.R
@@ -0,0 +1,15 @@
+test_hejke<- function(){
+    
+    # tutaj musi być wywołanie funkcji dla Katarzyny Hejke-Gójki
+    
+    name <- "Katarzyna Hejke Gójka"
+    age <- "45"
+    age_probal <- "0.5"
+    gender <- "kobieta"
+    gender_probal <- "0.8"
+    path_to_file <- "ścieżka do pliku"
+    
+    result = list("name" = name, "age" = age, "age_probal" = age_probal, "gender" = gender, "gender_probal" = gender_probal, "path" = path_to_file)
+    
+    return(result)
+}
