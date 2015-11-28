aab085e81d569d5a09272804107c5bb70b76feea
Wojtek Gumuła <voodkapl@gmail.com>
2015-11-28 14:40:02 +0100

    init

--- R/kondominium.R
+++
@@ -1,5 +0,0 @@
-kondominium <- function(niemieckie, rosyjskie, zydowski_zarzad_powierniczy) {
-  if(niemieckie && rosyjskie && zydowski_zarzad_powierniczy) {
-    browseURL("https://www.youtube.com/watch?v=iz7PhSSgMRc", browser = getOption("browser"))
-  }
-}
